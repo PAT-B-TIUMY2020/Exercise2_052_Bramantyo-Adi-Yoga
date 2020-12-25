2f0d2b3888015024bd07b26b54866263cedb26b5
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-12-25 23:47:07 +0700

    First Commit

---
+++ Exercise2_20180140052_Bramantyo Adi Yoga/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Service_Exercise2_052;
+using System.Net;
+
+namespace Exercise2_20180140052_Bramantyo_Adi_Yoga
+{
+    public partial class Form1 : Form
+    {
+        string baseUrl = 'http://localhost:1907/Mahasiswa';
+        void BuatMahasiswa()
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.nama = textBox1.Text;
+            mhs.nim = textBox2.Text;
+            mhs.prodi = textBox3.Text;
+            mhs.angkatan = textBox4.Text;
+            var data = JsonConvert.SerializeObject(mhs);
+            var postdata = new WebClient();
+            postdata.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            string response = postdata.UploadString(baseUrl + "Mahasiswa", data);
+            Console.WriteLine(response);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BuatMahasiswa();
+        }
+    }
+}
---
+++ ServerCode_Exercise2_052/Form1.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Service_Exercise2_052;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerCode_Exercise2_052
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Visible = true;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hostObject = null;
+
+            try
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    hostObject = new ServiceHost(typeof(Service));
+                    hostObject.Open();
+                });
+                label1.Text = "Server ON";
+                label2.Text = "Klik OFF untuk menonaktifkan server";
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                label2.Text = "Server Error";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hostObject.Abort();
+                label1.Text = "Server OFF";
+                label2.Text = "Klik ON untuk menghidupkan server";
+                button2.Enabled = false;
+                button1.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                label2.Text = "Server Error";
+            }
+        }
+    }
+}
